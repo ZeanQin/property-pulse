70ae16b57b4ce5aafbcf5c2a6b4a5989e0faef6a
Zean Qin <dev@zean.be>
2025-07-15 12:17:02 +1000

    Create layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import '@/assets/styles/globals.css'
 
+export const metadata = {
+    title: "PropertyPulse | Find the perfect property",
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties'
+};
+
 const MainLayout = ({children}) => {
     return (
         <html lang='en'>
